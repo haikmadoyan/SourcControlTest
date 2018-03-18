0b4022fcdef20ec5aec48881c29c2f2d88f3cc10
madoyanlab <haikmadoyan84@gmail.com>
2018-03-18 16:54:27 +0400

    version 1

--- SourcControlTest/ViewController.swift
+++ SourcControlTest/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // version 10
+        // version 1
     }
 
     override func didReceiveMemoryWarning() {
